80522f2d66161d40c30de2fc774316e8f7edf8c0
Jules Ecorchard <psn2jules@gmail.com>
2020-03-21 16:59:58 +0100

    count

--- js/count-visits.js
+++ js/count-visits.js
@@ -1,9 +1,11 @@
 const counter = document.getElementById('counter')
 
 const updateCounter = async () => {
+    if(window.location.href == "https://mon-entrainement-velo.netlify.com/prgmy.html") {
     const data = await fetch("https://api.countapi.xyz/hit/jules-e/visits");
     const count = await data.json();
-    console.log("visits: " + count.value)
+    console.log("visits: " + count.value - 5220)
+}
 };
 
 updateCounter();
