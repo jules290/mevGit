30a4e94b458e3c178fb21a1289c7b083d6828761
Jules Ecorchard <psn2jules@gmail.com>
2020-05-11 10:56:03 +0200

    1.5.25

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+$("#insta").hover(function(){
+    document.getElementById("insta").src = "./img/insta-blanc.png";
+    }, function(){
+    document.getElementById("insta").src = "./img/insta-gris.png";
+  });
