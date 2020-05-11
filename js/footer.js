112cff5e1b1324be64c2a03b641ffdc5fe071024
Jules Ecorchard <psn2jules@gmail.com>
2020-05-11 11:15:46 +0200

    1.5.26

--- js/footer.js
+++ js/footer.js
@@ -2,4 +2,10 @@ $("#insta").hover(function(){
     document.getElementById("insta").src = "./img/insta-blanc.png";
     }, function(){
     document.getElementById("insta").src = "./img/insta-gris.png";
-  });
+});
+
+$("#mail").hover(function(){
+    document.getElementById("mail").src = "./img/gmail-blanc.png";
+    }, function(){
+    document.getElementById("mail").src = "./img/gmail-gris.png";
+});
