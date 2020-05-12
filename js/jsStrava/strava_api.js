342285ceff4b58cf541d211b42c3c9d91584fc28
Jules Ecorchard <psn2jules@gmail.com>
2020-05-12 10:48:46 +0200

    1.6.2 test1

--- js/jsStrava/strava_api.js
+++ js/jsStrava/strava_api.js
@@ -4,33 +4,40 @@ let endPosition = window.location.href.indexOf("&scope");
 if (position != -1) {
     console.log(position + " " + endPosition)
     let code = window.location.href.slice(position, endPosition)
+    console.log(code)
     const auth_link = `https://www.strava.com/oauth/token?client_id=46262&client_secret=d10fe947c04ec802caa34e8f54f631090d305a77&code=${code}&grant_type=authorization_code`
     
     $.post(auth_link,
-    function(data){
-        $.get("https://www.strava.com/api/v3/athlete/activities?access_token=${data.access_token}", function(dataActivity){
-            console.log(dataActivity);
-        });
+    function(data, status){
+        console.log(data);
+        console.log(status)
+
     });
 }
 
+// function getActivities(res) {
+//     const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+//     fetch(activities_link)
+//         .then((res) => {
+//             console.log(res.json())
+//         })
+// }
 
+// function reAuthorize() {
+//     fetch(auth_link,{
+//         method: 'post',
+//         headers: {
+//             'Accept': 'application/json, text/plain, */*',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             client_id: '46262',
+//             client_secret: 'd10fe947c04ec802caa34e8f54f631090d305a77',
+//             refresh_token: '4bf09955b3267a17f660fedb2a4b16ed4700dfbf',
+//             grant_type: 'authorization_code'
+//         })
+//     }).then((res) => res.json())
+//         .then(res => getActivities(res))
+// }
 
-function reAuthorize() {
-    fetch(auth_link,{
-        method: 'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            client_id: '46262',
-            client_secret: 'd10fe947c04ec802caa34e8f54f631090d305a77',
-            refresh_token: '4bf09955b3267a17f660fedb2a4b16ed4700dfbf',
-            grant_type: 'authorization_code'
-        })
-    }).then((res) => res.json())
-        .then(res => getActivities(res))
-}
-
-reAuthorize()
+// reAuthorize()
