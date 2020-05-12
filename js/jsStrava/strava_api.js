2d9cbc5d0eeff7166ef3f7225ccc05d9370b82ed
Jules Ecorchard <psn2jules@gmail.com>
2020-05-12 12:32:17 +0200

    1.6.2 test3

--- js/jsStrava/strava_api.js
+++ js/jsStrava/strava_api.js
@@ -33,9 +33,9 @@ function reAuthorize() {
         body: JSON.stringify({
             client_id: '46262',
             client_secret: 'd10fe947c04ec802caa34e8f54f631090d305a77',
-            refresh_token: localStorage.refresh_token,
+            refresh_token: '4bf09955b3267a17f660fedb2a4b16ed4700dfbf',
             grant_type: 'refresh_token'
         })
-    }).then((res) => res.json())
-        .then(res => console.log(res))
+    }).then((res) => console.log(res.json()))
+        .then(res => localStorage.access_token = res)
 }
