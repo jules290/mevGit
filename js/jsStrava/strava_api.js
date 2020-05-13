b100e5184499bc67a437047d42d711f9e53a1fa8
Jules Ecorchard <psn2jules@gmail.com>
2020-05-13 07:37:13 +0200

    1.6.3

--- js/jsStrava/strava_api.js
+++ js/jsStrava/strava_api.js
@@ -5,6 +5,7 @@ if (position != -1) {
     let code = window.location.href.slice(position, endPosition);
     const auth_link = `https://www.strava.com/oauth/token?client_id=46262&client_secret=d10fe947c04ec802caa34e8f54f631090d305a77&code=${code}&grant_type=authorization_code`
     
+    console.log(localStorage.oauthStatus)
     if (localStorage.oauthStatus != "ok") {
         $.post(auth_link,
             function(data){
