81ac8922b517ba053a8cbc901d5786435dd1f44f
Jules Ecorchard <psn2jules@gmail.com>
2020-05-13 09:23:09 +0200

    1.6.3

--- js/jsStrava/strava_api.js
+++ js/jsStrava/strava_api.js
@@ -27,11 +27,30 @@ function getData() {
 }
 
 function getActivities() {
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities&per_page=10?access_token=${localStorage.access_token}`
-    fetch(activities_link)
-        .then((res) => {
-            console.log(res.json())
-        })
+    var StravaApiV3 = require('strava_api_v3');
+var defaultClient = StravaApiV3.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: strava_oauth
+var strava_oauth = defaultClient.authentications['strava_oauth'];
+strava_oauth.accessToken = "YOUR ACCESS TOKEN"
+
+var api = new StravaApiV3.ActivitiesApi()
+
+var opts = { 
+  'before': 56, // {Integer} An epoch timestamp to use for filtering activities that have taken place before a certain time.
+  'after': 56, // {Integer} An epoch timestamp to use for filtering activities that have taken place after a certain time.
+  'page': 56, // {Integer} Page number.
+  'perPage': 56 // {Integer} Number of items per page. Defaults to 30.
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+api.getLoggedInAthleteActivities(opts, callback);
 }
 
 const auth_linkRefresh = "https://www.strava.com/oauth/token";
