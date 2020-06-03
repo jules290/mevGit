11e214028fdb1a2b0ba5a147d8165ebd5fc5c95e
Jules Ecorchard <psn2jules@gmail.com>
2020-06-03 17:50:33 +0200

    1.6.9

--- js/difficulte.js
+++ js/difficulte.js
@@ -11,76 +11,180 @@ $(document).ready(function () {
         localStorage.lvlEndurance = 0;
         window.location.reload()
     })
+    sessionStorage.lvlSprint = localStorage.lvlSprint;
+    sessionStorage.lvlPMA = localStorage.lvlPMA;
+    sessionStorage.lvlSeuil = localStorage.lvlSeuil;
+    sessionStorage.lvlEndurance = localStorage.lvlEndurance;
+    sessionStorage.lvlGimenez = localStorage.lvlGimenez;
+    postDifficult();
+})
 
-    if(sessionStorage.lvlSprint >= 2) {
-        $("#STlvlProgressBar4").css("background-color", "white");
-    }
-    if(sessionStorage.lvlSprint >= 1) {
-        $("#STlvlProgressBar3").css("background-color", "white");
-    }
-    if(sessionStorage.lvlSprint >= 0) {
-        $("#STlvlProgressBar2").css("background-color", "white");
-    }
-    if(sessionStorage.lvlSprint >= -1) {
-        $("#STlvlProgressBar1").css("background-color", "white");
-    }
-
-
-    if(sessionStorage.lvlPMA >= 2) {
-        $("#PMAlvlProgressBar4").css("background-color", "white");
-    }
-    if(sessionStorage.lvlPMA >= 1) {
-        $("#PMAlvlProgressBar3").css("background-color", "white");
-    }
-    if(sessionStorage.lvlPMA >= 0) {
-        $("#PMAlvlProgressBar2").css("background-color", "white");
-    }
-    if(sessionStorage.lvlPMA >= -1) {
-        $("#PMAlvlProgressBar1").css("background-color", "white");
-    }
-
-
-    if(sessionStorage.lvlSeuil >= 2) {
-        $("#SeuillvlProgressBar4").css("background-color", "white");
-    }
-    if(sessionStorage.lvlSeuil >= 1) {
-        $("#SeuillvlProgressBar3").css("background-color", "white");
-    }
-    if(sessionStorage.lvlSeuil >= 0) {
-        $("#SeuillvlProgressBar2").css("background-color", "white");
-    }
-    if(sessionStorage.lvlSeuil >= -1) {
-        $("#SeuillvlProgressBar1").css("background-color", "white");
-    }
-    
-
-    if(sessionStorage.lvlGimenez >= 2) {
-        $("#GimenezlvlProgressBar4").css("background-color", "white");
-    }
-    if(sessionStorage.lvlGimenez >= 1) {
-        $("#GimenezlvlProgressBar3").css("background-color", "white");
-    }
-    if(sessionStorage.lvlGimenez >= 0) {
-        $("#GimenezlvlProgressBar2").css("background-color", "white");
-    }
-    if(sessionStorage.lvlGimenez >= -1) {
-        $("#GimenezlvlProgressBar1").css("background-color", "white");
-    }
-    
-
-    if(sessionStorage.lvlEndurance >= 2) {
-        $("#EndurancelvlProgressBar4").css("background-color", "white");
-    }
-    if(sessionStorage.lvlEndurance >= 1) {
-        $("#EndurancelvlProgressBar3").css("background-color", "white");
-    }
-    if(sessionStorage.lvlEndurance >= 0) {
-        $("#EndurancelvlProgressBar2").css("background-color", "white");
-    }
-    if(sessionStorage.lvlEndurance >= -1) {
-        $("#EndurancelvlProgressBar1").css("background-color", "white");
+$("#upSprint").click(function () {
+    if (sessionStorage.lvlSprint < 2) {
+        sessionStorage.lvlSprint++;
+        postDifficult();
     }
 })
+
+$("#downSprint").click(function () {
+    if (sessionStorage.lvlSprint > -2) {
+        sessionStorage.lvlSprint--;
+        postDifficult();
+    }
+})
+
+$("#upPMA").click(function () {
+    if (sessionStorage.lvlPMA < 2) {
+        sessionStorage.lvlPMA++;
+        postDifficult();
+    }
+})
+
+$("#downPMA").click(function () {
+    if (sessionStorage.lvlPMA > -2) {
+        sessionStorage.lvlPMA--;
+        postDifficult();
+    }
+})
+
+$("#upSeuil").click(function () {
+    if (sessionStorage.lvlSeuil < 2) {
+        sessionStorage.lvlSeuil++;
+        postDifficult();
+    }
+})
+
+$("#downSeuil").click(function () {
+    if (sessionStorage.lvlSeuil > -2) {
+        sessionStorage.lvlSeuil--;
+        postDifficult();
+    }
+})
+
+$("#upGimenez").click(function () {
+    if (sessionStorage.lvlGimenez < 2) {
+        sessionStorage.lvlGimenez++;
+        postDifficult();
+    }
+})
+
+$("#downGimenez").click(function () {
+    if (sessionStorage.lvlGimenez > -2) {
+        sessionStorage.lvlGimenez--;
+        postDifficult();
+    }
+})
+
+function postDifficult() {
+    const sprint = document.getElementById("sprintBar")
+    if(sessionStorage.lvlSprint == 2) {
+        sprint.classList.add("bar100PC");
+        sprint.classList.remove("bar0_75PC");
+        sprint.style.width = "auto";
+    }
+    else if(sessionStorage.lvlSprint == 1) {
+        sprint.classList.add("bar0_75PC");
+        sprint.classList.remove("bar100PC");
+        sprint.style.width = "75%";
+    }
+    else if(sessionStorage.lvlSprint == 0) {
+        sprint.classList.add("bar0_75PC");
+        sprint.classList.remove("bar100PC");
+        sprint.style.width = "50%";
+    }
+    else if(sessionStorage.lvlSprint == -1) {
+        sprint.classList.add("bar0_75PC");
+        sprint.classList.remove("bar100PC");
+        sprint.style.width = "25%";
+    }
+    else if(sessionStorage.lvlSprint == -2) {
+        sprint.classList.add("bar0_75PC");
+        sprint.classList.remove("bar100PC");
+        sprint.style.width = "0%";
+    }
+
+    const pma = document.getElementById("pmaBar")
+    if(sessionStorage.lvlPMA == 2) {
+        pma.classList.add("bar100PC");
+        pma.classList.remove("bar0_75PC");
+        pma.style.width = "auto";
+    }
+    else if(sessionStorage.lvlPMA == 1) {
+        pma.classList.add("bar0_75PC");
+        pma.classList.remove("bar100PC");
+        pma.style.width = "75%";
+    }
+    else if(sessionStorage.lvlPMA == 0) {
+        pma.classList.add("bar0_75PC");
+        pma.classList.remove("bar100PC");
+        pma.style.width = "50%";
+    }
+    else if(sessionStorage.lvlPMA == -1) {
+        pma.classList.add("bar0_75PC");
+        pma.classList.remove("bar100PC");
+        pma.style.width = "25%";
+    }
+    else if(sessionStorage.lvlPMA == -2) {
+        pma.classList.add("bar0_75PC");
+        pma.classList.remove("bar100PC");
+        pma.style.width = "0%";
+    }
+
+    const seuil = document.getElementById("seuilBar")
+    if(sessionStorage.lvlSeuil == 2) {
+        seuil.classList.add("bar100PC");
+        seuil.classList.remove("bar0_75PC");
+        seuil.style.width = "auto";
+    }
+    else if(sessionStorage.lvlSeuil == 1) {
+        seuil.classList.add("bar0_75PC");
+        seuil.classList.remove("bar100PC");
+        seuil.style.width = "75%";
+    }
+    else if(sessionStorage.lvlSeuil == 0) {
+        seuil.classList.add("bar0_75PC");
+        seuil.classList.remove("bar100PC");
+        seuil.style.width = "50%";
+    }
+    else if(sessionStorage.lvlSeuil == -1) {
+        seuil.classList.add("bar0_75PC");
+        seuil.classList.remove("bar100PC");
+        seuil.style.width = "25%";
+    }
+    else if(sessionStorage.lvlSeuil == -2) {
+        seuil.classList.add("bar0_75PC");
+        seuil.classList.remove("bar100PC");
+        seuil.style.width = "0%";
+    }
+    
+
+    const gimenez = document.getElementById("gimenezBar")
+    if(sessionStorage.lvlGimenez == 2) {
+        gimenez.classList.add("bar100PC");
+        gimenez.classList.remove("bar0_75PC");
+        gimenez.style.width = "auto";
+    }
+    else if(sessionStorage.lvlGimenez == 1) {
+        gimenez.classList.add("bar0_75PC");
+        gimenez.classList.remove("bar100PC");
+        gimenez.style.width = "75%";
+    }
+    else if(sessionStorage.lvlGimenez == 0) {
+        gimenez.classList.add("bar0_75PC");
+        gimenez.classList.remove("bar100PC");
+        gimenez.style.width = "50%";
+    }
+    else if(sessionStorage.lvlGimenez == -1) {
+        gimenez.classList.add("bar0_75PC");
+        gimenez.classList.remove("bar100PC");
+        gimenez.style.width = "25%";
+    }
+    else if(sessionStorage.lvlGimenez == -2) {
+        gimenez.classList.add("bar0_75PC");
+        gimenez.classList.remove("bar100PC");
+        gimenez.style.width = "0%";
+    }
+}
 
 $("#sauvegarder").click(function () {
     ShowAlertPerso()
@@ -102,202 +206,6 @@ $("#non").click(function () {
 $("#filter").click(function () {
     hideAlertPerso()
 })
-
-sessionStorage.lvlSprint = localStorage.lvlSprint;
-sessionStorage.lvlPMA = localStorage.lvlPMA;
-sessionStorage.lvlSeuil = localStorage.lvlSeuil;
-sessionStorage.lvlEndurance = localStorage.lvlEndurance;
-sessionStorage.lvlGimenez = localStorage.lvlGimenez;
-$("#upSprint").click(function () {
-    if (sessionStorage.lvlSprint < 2) {
-        sessionStorage.lvlSprint++;
-
-        if(sessionStorage.lvlSprint >= 2) {
-            $("#STlvlProgressBar4").css("background-color", "white");
-        }
-        if(sessionStorage.lvlSprint >= 1) {
-            $("#STlvlProgressBar3").css("background-color", "white");
-        }
-        if(sessionStorage.lvlSprint >= 0) {
-            $("#STlvlProgressBar2").css("background-color", "white");
-        }
-        if(sessionStorage.lvlSprint >= -1) {
-            $("#STlvlProgressBar1").css("background-color", "white");
-        }
-    }
-})
-
-$("#downSprint").click(function () {
-    if (sessionStorage.lvlSprint > -2) {
-        sessionStorage.lvlSprint--;
-
-        if(sessionStorage.lvlSprint < 2) {
-            $("#STlvlProgressBar4").css("background-color", "");
-        }
-        if(sessionStorage.lvlSprint < 1) {
-            $("#STlvlProgressBar3").css("background-color", "");
-        }
-        if(sessionStorage.lvlSprint < 0) {
-            $("#STlvlProgressBar2").css("background-color", "");
-        }
-        if(sessionStorage.lvlSprint < -1) {
-            $("#STlvlProgressBar1").css("background-color", "");
-        }
-    }
-})
-
-$("#upPMA").click(function () {
-    if (sessionStorage.lvlPMA < 2) {
-        sessionStorage.lvlPMA++;
-
-        if(sessionStorage.lvlPMA >= 2) {
-            $("#PMAlvlProgressBar4").css("background-color", "white");
-        }
-        if(sessionStorage.lvlPMA >= 1) {
-            $("#PMAlvlProgressBar3").css("background-color", "white");
-        }
-        if(sessionStorage.lvlPMA >= 0) {
-            $("#PMAlvlProgressBar2").css("background-color", "white");
-        }
-        if(sessionStorage.lvlPMA >= -1) {
-            $("#PMAlvlProgressBar1").css("background-color", "white");
-        }
-    }
-})
-
-$("#downPMA").click(function () {
-    if (sessionStorage.lvlPMA > -2) {
-        sessionStorage.lvlPMA--;
-
-        if(sessionStorage.lvlPMA < 2) {
-            $("#PMAlvlProgressBar4").css("background-color", "");
-        }
-        if(sessionStorage.lvlPMA < 1) {
-            $("#PMAlvlProgressBar3").css("background-color", "");
-        }
-        if(sessionStorage.lvlPMA < 0) {
-            $("#PMAlvlProgressBar2").css("background-color", "");
-        }
-        if(sessionStorage.lvlPMA < -1) {
-            $("#PMAlvlProgressBar1").css("background-color", "");
-        }
-    }
-})
-
-$("#upSeuil").click(function () {
-    if (sessionStorage.lvlSeuil < 2) {
-        sessionStorage.lvlSeuil++;
-
-        if(sessionStorage.lvlSeuil >= 2) {
-            $("#SeuillvlProgressBar4").css("background-color", "white");
-        }
-        if(sessionStorage.lvlSeuil >= 1) {
-            $("#SeuillvlProgressBar3").css("background-color", "white");
-        }
-        if(sessionStorage.lvlSeuil >= 0) {
-            $("#SeuillvlProgressBar2").css("background-color", "white");
-        }
-        if(sessionStorage.lvlSeuil >= -1) {
-            $("#SeuillvlProgressBar1").css("background-color", "white");
-        }
-    }
-})
-
-$("#downSeuil").click(function () {
-    if (sessionStorage.lvlSeuil > -2) {
-        sessionStorage.lvlSeuil--;
-        
-        if(sessionStorage.lvlSeuil < 2) {
-            $("#SeuillvlProgressBar4").css("background-color", "");
-        }
-        if(sessionStorage.lvlSeuil < 1) {
-            $("#SeuillvlProgressBar3").css("background-color", "");
-        }
-        if(sessionStorage.lvlSeuil < 0) {
-            $("#SeuillvlProgressBar2").css("background-color", "");
-        }
-        if(sessionStorage.lvlSeuil < -1) {
-            $("#SeuillvlProgressBar1").css("background-color", "");
-        }
-    }
-})
-
-$("#upGimenez").click(function () {
-    if (sessionStorage.lvlGimenez < 2) {
-        sessionStorage.lvlGimenez++;
-
-        if(sessionStorage.lvlGimenez >= 2) {
-            $("#GimenezlvlProgressBar4").css("background-color", "white");
-        }
-        if(sessionStorage.lvlGimenez >= 1) {
-            $("#GimenezlvlProgressBar3").css("background-color", "white");
-        }
-        if(sessionStorage.lvlGimenez >= 0) {
-            $("#GimenezlvlProgressBar2").css("background-color", "white");
-        }
-        if(sessionStorage.lvlGimenez >= -1) {
-            $("#GimenezlvlProgressBar1").css("background-color", "white");
-        }
-    }
-})
-
-$("#downGimenez").click(function () {
-    if (sessionStorage.lvlGimenez > -2) {
-        sessionStorage.lvlGimenez--;
-        
-        if(sessionStorage.lvlGimenez < 2) {
-            $("#GimenezlvlProgressBar4").css("background-color", "");
-        }
-        if(sessionStorage.lvlGimenez < 1) {
-            $("#GimenezlvlProgressBar3").css("background-color", "");
-        }
-        if(sessionStorage.lvlGimenez < 0) {
-            $("#GimenezlvlProgressBar2").css("background-color", "");
-        }
-        if(sessionStorage.lvlGimenez < -1) {
-            $("#GimenezlvlProgressBar1").css("background-color", "");
-        }
-    }
-})
-
-// $("#upEndurance").click(function () {
-//     if (sessionStorage.lvlEndurance <2) {
-//         sessionStorage.lvlEndurance++;
-
-//         if(sessionStorage.lvlEndurance >= 2) {
-//             $("#EndurancelvlProgressBar4").css("background-color", "white");
-//         }
-//         if(sessionStorage.lvlEndurance >= 1) {
-//             $("#EndurancelvlProgressBar3").css("background-color", "white");
-//         }
-//         if(sessionStorage.lvlEndurance >= 0) {
-//             $("#EndurancelvlProgressBar2").css("background-color", "white");
-//         }
-//         if(sessionStorage.lvlEndurance >= -1) {
-//             $("#EndurancelvlProgressBar1").css("background-color", "white");
-//         }
-//     }
-// })
-
-// $("#downEndurance").click(function () {
-//     if (sessionStorage.lvlEndurance > -2) {
-//         sessionStorage.lvlEndurance--;
-
-//         if(sessionStorage.lvlEndurance < 2) {
-//             $("#EndurancelvlProgressBar4").css("background-color", "");
-//         }
-//         if(sessionStorage.lvlEndurance < 1) {
-//             $("#EndurancelvlProgressBar3").css("background-color", "");
-//         }
-//         if(sessionStorage.lvlEndurance < 0) {
-//             $("#EndurancelvlProgressBar2").css("background-color", "");
-//         }
-//         if(sessionStorage.lvlEndurance < -1) {
-//             $("#EndurancelvlProgressBar1").css("background-color", "");
-//         }
-//     }  
-// })
-
 
 function ShowAlertPerso() {
     $("#alert").animate({height: '170px'}, 200);
