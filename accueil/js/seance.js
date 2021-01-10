1dd586417d1bf2ef42e7f108072b53b7294a5bf0
Jules Ecorchard <psn2jules@gmail.com>
2021-01-10 13:02:25 +0100

    2.0

--- accueil/js/seance.js
+++ accueil/js/seance.js
@@ -7,7 +7,7 @@ function postSeance() {
     else {
         document.getElementById("SEANCES_BOX").className = "case2";
         document.getElementById("SEANCES_BOX").innerHTML = 
-            "<h2 id='suggestionSeance'>TROUVER LA SEANCE QUI VOUS FERRA PROGRESSER AUJOURD'HUI</h2>"+
+            "<h2 id='suggestionSeance'>TROUVEZ LA SEANCE QUI VOUS FERRA PROGRESSER AUJOURD'HUI</h2>"+
             '<button id="suggestionSeanceBtn">TROUVER UNE SEANCE</button>'
         $("#suggestionSeanceBtn").click(function () {window.location.href = '../programme/programme.html'})
     }
